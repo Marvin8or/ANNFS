942c12155548bd491092cb40cbd887836e51e2df
GabrielM <GMarvin@dspace.hr>
2023-06-28 09:38:35 +0200

    Implemented SetError Method and MSEError method

---
+++ NeuralNetwork/SetErrors.cpp
@@ -0,0 +1,49 @@
+#include  "NeuralNetwork.h"
+
+void NeuralNetwork::setErrors()
+{
+	if(this->target.size() == 0)
+	{
+		cerr << "No target for this neural network" << endl;
+		assert(false);
+	}
+
+	int outputLayerSize = this->layers.at(this->layers.size() - 1)->getNeurons().size();
+	if(this->target.size() != outputLayerSize)
+	{
+		cerr << "Target size (" << this->target.size() << ") is not the same as output layer size: " << outputLayerSize << endl;
+		for (int i = 0; i < this->target.size(); i++)
+		{
+			cout << this->target.at(i) << endl;
+		}
+	}
+
+	switch (this->costFunction)
+	{
+		case(COST_MSE):
+			this->setErrorMSE();
+			break;
+		default:
+			this->setErrorMSE();
+			break;	
+	}
+}
+
+
+void NeuralNetwork::setErrorMSE()
+{
+	int outputLayerIndex = this->layers.size() - 1;
+	vector<Neuron*> outputNeurons = this->layers.at(outputLayerIndex)->getNeurons();
+
+	this->error = 0.00;
+	for(int i = 0; i < target.size();i++)
+	{
+		double t = target.at(i);
+		double y = outputNeurons.at(i)->getActivatedValue();
+
+		errors.at(i) = pow(abs(t - y), 2);
+		derivedErrors.at(i) = (y - t);
+
+		this->error += errors.at(i);
+	}
+}
