2a44bc17d2aa5b1fb2f9fe61d01156716f0f324d
GabrielM <GMarvin@dspace.hr>
2023-07-15 14:53:32 +0200

    Updated backPropagation function

--- NeuralNetwork/BackPropagation.cpp
+++ NeuralNetwork/BackPropagation.cpp
@@ -5,8 +5,8 @@
 void NeuralNetwork::backPropagation()
 {
 	// Output layer
-	int outputLayerIndex = this->topology.size() - 1;
-	int lastWeightMatrixIndex = outputLayerIndex - 1;
+	int outputLayerIndex = topology.size() - 1;
+	int lastWeightMatrixIndex = weightMatrices.size() - 1;
 
 	vector<Matrix*> vector_dC0_cw;
 	vector<Matrix*> newWeights;
@@ -14,38 +14,39 @@ void NeuralNetwork::backPropagation()
 
 	// Calculate the vector of derived cost functions
 	Matrix* derivedCostFunction = new Matrix(this->layers.at(outputLayerIndex)->getSize(), 1, false);
-	for(int r = 0; r < this->layers.at(outputLayerIndex)->getSize(); r++)
+	for(int r = 0; r < layers.at(outputLayerIndex)->getSize(); r++)
 	{
-		derivedCostFunction->setValue(r, 0, 2 * this->getErrors().at(r));
+		derivedCostFunction->setValue(r, 0, 2 * getErrors().at(r));
 	}
 	vector_dC0_ca.push_back(derivedCostFunction);
-	Matrix* derivedLayer = this->layers.at(outputLayerIndex)->matrixifyDerivedValues()->transpose();
 
-	Matrix* hadamardProduct;
+	Matrix* derivedLayer = layers.at(outputLayerIndex)->matrixifyDerivedValues();
+
+	Matrix* hadamardProduct = new Matrix(derivedCostFunction->getNumRows(), derivedCostFunction->getNumCols(), false);
 	Operations::HadamardProduct(derivedLayer, derivedCostFunction, hadamardProduct);
-	hadamardProduct->transpose();
 
-	Matrix* activatedPreviousLayer = this->layers.at(outputLayerIndex - 1)->matrixifyActivatedValues()->transpose();
+	Matrix* activatedPreviousLayer = layers.at(outputLayerIndex - 1)->matrixifyActivatedValues()->transpose();
 
-	Matrix* dC0_cw;
-	Operations::MultiplyMatrices(activatedPreviousLayer, hadamardProduct, dC0_cw);
-	vector_dC0_cw.push_back(dC0_cw);
+	Matrix* dC0_cw = new Matrix(hadamardProduct->getNumRows(), activatedPreviousLayer->getNumCols(), false);
+	Operations::MultiplyMatrices(hadamardProduct, activatedPreviousLayer, dC0_cw);
+	vector_dC0_cw.push_back(dC0_cw->transpose());
 
 	// Calculate the new weight matrix between output layer and last hidden layer
-	Matrix* newWeight;
-	Operations::SubtractMatrices(this->weightMatrices.at(lastWeightMatrixIndex), dC0_cw, newWeight);
+	Matrix* newWeight = new Matrix(dC0_cw->getNumCols(), dC0_cw->getNumRows(), false);
+	Operations::SubtractMatrices(weightMatrices.at(lastWeightMatrixIndex), dC0_cw->transpose(), newWeight); //TODO Multiply with learning rate
 	newWeights.push_back(newWeight);
+	
 
 	// Move from last hidden to input layer
 	for(int layer_index = outputLayerIndex - 1; layer_index > 0; layer_index--)
 	{
 		// Calculate dC0_da_L-1
-		Matrix* weightRightOfCurrentLayer = this->weightMatrices.at(layer_index);
+		Matrix* weightRightOfCurrentLayer = weightMatrices.at(layer_index);
 		Matrix* previousLayerError = vector_dC0_ca.back();
-		derivedLayer = this->layers.at(layer_index + 1)->matrixifyDerivedValues()->transpose();
+		derivedLayer = layers.at(layer_index + 1)->matrixifyDerivedValues();
 
 		Operations::HadamardProduct(derivedLayer, previousLayerError, hadamardProduct);
-		Matrix* thisLayerError;
+		Matrix* thisLayerError = new Matrix(weightRightOfCurrentLayer->getNumRows(), hadamardProduct->getNumCols(), false);
 		Operations::MultiplyMatrices(weightRightOfCurrentLayer, hadamardProduct, thisLayerError);
 		vector_dC0_ca.push_back(thisLayerError);
 
@@ -54,21 +55,23 @@ void NeuralNetwork::backPropagation()
 		// Check if next layer is input layer
 		if(layer_index - 1 == 0)
 		{
-			activatedPreviousLayer = this->layers.at(layer_index - 1)->matrixifyValues()->transpose();
-			derivedLayer = new Matrix(thisLayerError->getNumRows(), thisLayerError->getNumCols(), false, 1.0);
+			activatedPreviousLayer = layers.at(layer_index - 1)->matrixifyValues()->transpose();
+			derivedLayer = new Matrix(thisLayerError->getNumRows(), thisLayerError->getNumCols(), false);
 		}
 		else
 		{
 			activatedPreviousLayer = this->layers.at(layer_index)->matrixifyActivatedValues()->transpose();
-			derivedLayer = this->layers.at(layer_index - 1)->matrixifyDerivedValues()->transpose();
+			derivedLayer = layers.at(layer_index - 1)->matrixifyDerivedValues();
 		}
+		hadamardProduct = new Matrix(thisLayerError->getNumRows(), thisLayerError->getNumCols(), false);
 		Operations::HadamardProduct(derivedLayer, thisLayerError, hadamardProduct);
-		hadamardProduct->transpose();
 
-		Operations::MultiplyMatrices(activatedPreviousLayer, hadamardProduct, dC0_cw);
-		vector_dC0_cw.push_back(dC0_cw);
+		dC0_cw = new Matrix(hadamardProduct->getNumRows(), activatedPreviousLayer->getNumCols(), false);
+		Operations::MultiplyMatrices(hadamardProduct, activatedPreviousLayer, dC0_cw);
+		vector_dC0_cw.push_back(dC0_cw->transpose());
 
-		Operations::SubtractMatrices(this->weightMatrices.at(layer_index - 1), dC0_cw, newWeight);
+		newWeight = new Matrix(dC0_cw->getNumCols(), dC0_cw->getNumRows(), false);
+		Operations::SubtractMatrices(weightMatrices.at(layer_index - 1), vector_dC0_cw.back(), newWeight);
 		newWeights.push_back(newWeight);
 
 		// Free up pointers
@@ -77,14 +80,13 @@ void NeuralNetwork::backPropagation()
 		delete thisLayerError;
 	}
 
-	delete derivedCostFunction;
+	
 	delete derivedLayer;
 	delete hadamardProduct;
 	delete activatedPreviousLayer;
 	delete dC0_cw;
-	delete newWeight;
 
 
 	reverse(newWeights.begin(), newWeights.end());
-	this->weightMatrices = newWeights;
+	weightMatrices = newWeights;
 }
